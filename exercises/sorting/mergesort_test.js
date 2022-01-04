c317642ca11b4b3876c69236e3f104d72be127a9
Prakhar <prakharpalpfdr+github@gmail.com>
2022-01-05 00:04:13 +0530

    Uncommited files from Nov 30, 2018

---
+++ exercises/sorting/mergesort_test.js
@@ -0,0 +1,2 @@
+const {mergeSort} = require('./index');
+console.log(mergeSort([100, -40, 500, -124, 0, 21, 7]));
---
+++ exercises/sorting/use.js
@@ -0,0 +1,4 @@
+const {merge}  = require('./index');
+// console.log('required the function');
+// console.log(merge([1,10],[2,8,12]));
+console.log(merge([0,21],[7]));
